84df1d0a5a6d63d4c890537af372d9cb5f07b04d
André Alves <andrealvesdc@gmail.com>
2020-05-27 22:59:06 -0300

    Create project

---
+++ src/main/java/com/andrealves/apirestfullspringmongo/ApirestfullspringmongoApplication.java
@@ -0,0 +1,13 @@
+package com.andrealves.apirestfullspringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestfullspringmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestfullspringmongoApplication.class, args);
+	}
+
+}
