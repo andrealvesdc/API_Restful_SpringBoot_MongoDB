0183df42dffe64a8912c8250279e5bcbdeceb37d
André Alves <andrealvesdc@gmail.com>
2020-05-30 20:45:50 -0300

    Projeção dos dados do author com DTO

--- src/main/java/com/andrealves/apirestfullspringmongo/config/Instantiation.java
+++ src/main/java/com/andrealves/apirestfullspringmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.andrealves.apirestfullspringmongo.domain.Post;
 import com.andrealves.apirestfullspringmongo.domain.User;
+import com.andrealves.apirestfullspringmongo.dto.AuthorDTO;
 import com.andrealves.apirestfullspringmongo.repository.PostRepository;
 import com.andrealves.apirestfullspringmongo.repository.UserRepository;
 
@@ -34,11 +35,11 @@ public class Instantiation implements CommandLineRunner{
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para são paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para são paulo. Abraços!", maria);
-		
 		useReposotory.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para são paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "O dia esta...", new AuthorDTO(maria));
+		
 		postReposotory.saveAll(Arrays.asList(post1, post2));
 		
 	}
