513a8ddf9188c63809776cfd9baa6a2f5201a788
André Alves <andrealvesdc@gmail.com>
2020-05-30 21:22:49 -0300

    Obtendo post por ID

---
+++ src/main/java/com/andrealves/apirestfullspringmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.andrealves.apirestfullspringmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andrealves.apirestfullspringmongo.domain.Post;
+import com.andrealves.apirestfullspringmongo.repository.PostRepository;
+import com.andrealves.apirestfullspringmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll(){
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
