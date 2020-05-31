a227d221d2ea0f9535d583b115b13055fe0424d5
André Alves <andrealvesdc@gmail.com>
2020-05-31 11:24:53 -0300

    Adicionando comentarios aos posts

--- src/main/java/com/andrealves/apirestfullspringmongo/config/Instantiation.java
+++ src/main/java/com/andrealves/apirestfullspringmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.andrealves.apirestfullspringmongo.domain.Post;
 import com.andrealves.apirestfullspringmongo.domain.User;
 import com.andrealves.apirestfullspringmongo.dto.AuthorDTO;
+import com.andrealves.apirestfullspringmongo.dto.CommentDTO;
 import com.andrealves.apirestfullspringmongo.repository.PostRepository;
 import com.andrealves.apirestfullspringmongo.repository.UserRepository;
 
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para são paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "O dia esta...", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/02/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2020"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um otimo dia", sdf.parse("25/04/2019"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postReposotory.saveAll(Arrays.asList(post1, post2));
 		
